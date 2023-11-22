7123a83345a5102ba34a22301d7b3fe3bdb362da
zGiusee <giusebelforte@gmail.com>
2023-11-22 15:31:30 +0100

    Aggiunti i commenti in italiano

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,40 +1,52 @@
 
-// DEFINISCO LA LISTA IN COSTANTE
+// DEFINISCO LA LISTA IN UNA COSTANTE
 const lista = document.querySelector("ul");
 
 // CREO L'ALGORITMO 
 
 for(let i=1; i<=100; i++){
    
-    // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI SIA DI 3 CHE DI 5 OVVERO 15
+    // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI SIA DI 3 CHE DI 5
     if(i%15 == 0){
+        // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
         let li;
         li = `<li id=num${i}>FizzBuzz</li>`;
+
+        // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
-        console.log("FizzBuzz");
+        // console.log("FizzBuzz");
     }
 
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI DI 3
     else if(i%5 == 0){
+        // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
         let li;
         li = `<li id=num${i}>Buzz</li>`;
+
+         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
-        console.log("Buzz");
+        // console.log("Buzz");
     }
 
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI DI 5
     else if (i%3 == 0){
+        // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
         let li;
         li = `<li id=num${i}>Fizz</li>`;
+
+         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
-        console.log("Fizz");
+        // console.log("Fizz");
     }
 
     else{
+        // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
         let li;
         li = `<li id=num${i}>${i}</li>`;
+
+         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
-        console.log(i)
+        // console.log(i)
     }
     
 }
