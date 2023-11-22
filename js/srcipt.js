57d71fdd2371cc8695cbcc32b8c755230fbbba38
zGiusee <giusebelforte@gmail.com>
2023-11-22 15:12:54 +0100

    tryin to make the li insade the html "ul"

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,25 +1,34 @@
 
-
+// DEFINISCO LA LISTA IN COSTANTE
+const lista = document.getElementById("lista");
 // CREO L'ALGORITMO 
 
 for(let i=1; i<=100; i++){
    
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI SIA DI 3 CHE DI 5 OVVERO 15
     if(i%15 == 0){
-        console.log("FizzBuzz")
+        let li = `<li id=num${i}>FizzBuzz</li>`;
+        lista.innerHTML = + li;
+        console.log("FizzBuzz");
     }
 
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI DI 3
     else if(i%5 == 0){
+        let li = `<li id=num${i}>Buzz</li>`;
+        lista.innerHTML = + li;
         console.log("Buzz");
     }
 
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI DI 5
     else if (i%3 == 0){
+        let li = `<li id=num${i}>Fizz</li>`;
+        lista.innerHTML = + li;
         console.log("Fizz");
     }
 
     else{
+        let li = `<li id=num${i}>${i}</li>`;
+        lista.innerHTML = + li;
         console.log(i)
     }
     
