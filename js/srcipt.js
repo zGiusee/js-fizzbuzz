29018183d7ffa61d5da79f28fc9bddb1aa0cf45a
zGiusee <giusebelforte@gmail.com>
2023-11-22 15:32:55 +0100

    Corretto calcolo della prima variabile (100% don )

--- js/srcipt.js
+++ js/srcipt.js
@@ -7,12 +7,12 @@ const lista = document.querySelector("ul");
 for(let i=1; i<=100; i++){
    
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI SIA DI 3 CHE DI 5
-    if(i%15 == 0){
+    if(i%3 == 0 && i%5 == 0){
         // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
         let li;
         li = `<li id=num${i}>FizzBuzz</li>`;
 
-        // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
+        // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
         // console.log("FizzBuzz");
     }
@@ -23,7 +23,7 @@ for(let i=1; i<=100; i++){
         let li;
         li = `<li id=num${i}>Buzz</li>`;
 
-         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
+         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
         // console.log("Buzz");
     }
@@ -34,7 +34,7 @@ for(let i=1; i<=100; i++){
         let li;
         li = `<li id=num${i}>Fizz</li>`;
 
-         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
+         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
         // console.log("Fizz");
     }
@@ -44,7 +44,7 @@ for(let i=1; i<=100; i++){
         let li;
         li = `<li id=num${i}>${i}</li>`;
 
-         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA PRECEDENTEMENTE INDICATA NELLA COSTANTE
+         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
         // console.log(i)
     }
