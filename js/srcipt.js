bab10ea0416ae59f5d8a1e7e4faf7661474894f0
zGiusee <giusebelforte@gmail.com>
2023-11-23 09:37:18 +0100

    Removed the li declaretion and made it in one row

--- js/srcipt.js
+++ js/srcipt.js
@@ -9,8 +9,7 @@ for(let i=1; i<=100; i++){
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI SIA DI 3 CHE DI 5
     if(i%3 == 0 && i%5 == 0){
         // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
-        let li;
-        li = `<li id=num${i}>FizzBuzz</li>`;
+        let li = `<li id=num${i}>FizzBuzz</li>`;
 
         // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
@@ -20,8 +19,7 @@ for(let i=1; i<=100; i++){
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI DI 3
     else if(i%5 == 0){
         // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
-        let li;
-        li = `<li id=num${i}>Buzz</li>`;
+        let li = `<li id=num${i}>Buzz</li>`;
 
          // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
@@ -31,8 +29,7 @@ for(let i=1; i<=100; i++){
     // ESEGUO IL CALCOLO DEL RESTO PER I MULTIPLI DI 5
     else if (i%3 == 0){
         // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
-        let li;
-        li = `<li id=num${i}>Fizz</li>`;
+        let li = `<li id=num${i}>Fizz</li>`;
 
          // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
@@ -41,8 +38,7 @@ for(let i=1; i<=100; i++){
 
     else{
         // DICHIARO LA VARIABILE "LI" E AGGIUNGO IL CONTENUTO CHE DOVRà AVERE
-        let li;
-        li = `<li id=num${i}>${i}</li>`;
+        let li = `<li id=num${i}>${i}</li>`;
 
          // AGGIUNGO QUINDI "LI" DENTRO LA LISTA, PRECEDENTEMENTE INDICATA NELLA COSTANTE
         lista.innerHTML += li;
